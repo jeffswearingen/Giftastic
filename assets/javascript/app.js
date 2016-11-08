419d405ce069b623b5e5950dae2ba63ef01349c9
Jeff Swearingen <jeffswearingen@gmail.com>
2016-11-08 10:05:56 -0500

    develop query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 var arrayOfButtons = ["kirk", "spock", "sulu", "uhura", "scotty", "klingon", "vulcan", "picard", "riker", "enterprise"];
+var apiKey = "dc6zaTOxFJmzC";
+
 
 function renderButtons() {
 	$(".clickable").remove();
@@ -13,17 +15,33 @@ function renderButtons() {
 
 $(document).ready(function() {
 	renderButtons();
-});
 
-$('#addPicture').on('click', function() {
-	console.log($("input#picture-input").val());
 
-	var inputPicture = $("input#picture-input").val();
+	$('#addPicture').on('click', function() {
+		console.log($("input#picture-input").val());
+
+		var inputPicture = $("input#picture-input").val().trim();
 		arrayOfButtons.push(inputPicture);
 
 		//if (arrayOfButtons.indexOf(inputPicture) !== -1) {
 		//	arrayOfButtons.push(inputPicture);
 		//}
-	renderButtons();
-	return false;
+		renderButtons();
+		return false;
+	});
+
+	$('.clickable').on('click', function() {
+		var query = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&r=pg&limit=5"   
+  	query += "&q=" + $(this).text();
+	  console.log(query);
+	  $.ajax( {
+	  	url: query,
+	  	method: 'GET' }
+	  	.done(function(response) {
+	  		
+	  	})
+	  })
+	});
 });
+
+
