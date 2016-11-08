1502b09e4f6704850333abda34ee8beb0abc1c6b
Jeff Swearingen <jeffswearingen@gmail.com>
2016-11-08 13:08:06 -0500

    display still images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,32 @@ function renderButtons() {
 		buttonNew.text(arrayOfButtons[i]);
 		$(".selectorButtons").append(buttonNew);
 	}
+	$('.clickable').on('click', function() {
+		var query = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&r=pg&limit=5"   
+  	query += "&q=" + $(this).text();
+	  console.log(query);
+	  $.ajax({
+	  	url: query,
+	  	method: 'GET', 
+	  }).done(function(response) {
+	  		console.log(response);
+	  		for (var i = 0; i < response.data.length; i++) {
+	  			console.log(response.data[i]);
+	  			var tempBlock = $("<div>");
+	  			var tempImage = $("<img>");
+	  			tempImage.attr('src', response.data[i].images.original_still.url);
+//	  			tempBlock.attr("html", "<img src=" + response.data[i].images.original_still.url + ">");
+					$(tempBlock).append(tempImage);
+					var tempRating = $("<p>");
+					tempRating.text("Rating: " + response.data[i].rating);
+					$(tempBlock).append(tempRating);
+	  			$(".pictures").append(tempBlock);
+	  		}
+		}).fail(function(err) {
+			throw err;
+		});
+		console.log("exit");
+	});
 //	return false;
 }
 
@@ -30,20 +56,7 @@ $(document).ready(function() {
 		return false;
 	});
 
-	$('.clickable').on('click', function() {
-		var query = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&r=pg&limit=5"   
-  	query += "&q=" + $(this).text();
-	  console.log(query);
-	  $.ajax({
-	  	url: query,
-	  	method: 'GET', 
-	  }).done(function(response) {
-	  		console.log(response)
-		}).fail(function(err) {
-			throw err;
-		});
-		console.log("exit");
-	});
+	
 })
 
 
