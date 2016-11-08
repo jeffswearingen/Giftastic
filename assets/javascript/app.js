4058da022fec58721674bb0eb34954dc8a058afd
Jeff Swearingen <jeffswearingen@gmail.com>
2016-11-07 20:45:33 -0500

    Button fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,29 @@
 var arrayOfButtons = ["kirk", "spock", "sulu", "uhura", "scotty", "klingon", "vulcan", "picard", "riker", "enterprise"];
 
 function renderButtons() {
-	$(".clickable").empty();
+	$(".clickable").remove();
 	for (var i = 0; i < arrayOfButtons.length; i++) {
 		var buttonNew = $("<button>");
 		buttonNew.attr("class", "clickable");
 		buttonNew.text(arrayOfButtons[i]);
 		$(".selectorButtons").append(buttonNew);
 	}
-	return false;
+//	return false;
 }
 
 $(document).ready(function() {
 	renderButtons();
+});
 
-	$('#addPicture').on('submit', function() {
-		val inputPicture = $("input#picture-input").val();
-		if (arrayOfButtons.indexOf(inputPicture) !== -1) {
-			arrayOfButtons.push(inputPicture);
-		}
-		renderButtons();
-	});
+$('#addPicture').on('click', function() {
+	console.log($("input#picture-input").val());
+
+	var inputPicture = $("input#picture-input").val();
+		arrayOfButtons.push(inputPicture);
+
+		//if (arrayOfButtons.indexOf(inputPicture) !== -1) {
+		//	arrayOfButtons.push(inputPicture);
+		//}
+	renderButtons();
+	return false;
 });
